7a740248f613ce3df8fd6e109430a88eeac6b94f
huysun <huysun2511@gmail.com>
2025-08-30 22:34:54 +0700

    them admin

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -21,49 +21,52 @@ export default function UserLayout() {
             <div className="header">
                 <Container2>
                     <div className="header__wrap">
-                        <Link to=""><div className='header__logo'><img src={logo} alt="logo.png" /></div></Link>
+                        <Link to="/">
+                            <div className='header__logo'>
+                                <img src={logo} alt="logo.png" />
+                            </div>
+                        </Link>
+
                         <ul>
                             <li><NavLink to="/curriculum">Chương trình học</NavLink></li>
                             <li><NavLink to="/blog">Blog</NavLink></li>
                             <li><NavLink to="/feedback">Feedback</NavLink></li>
                             <li><NavLink to="/aboutus">Về chúng tôi</NavLink></li>
                         </ul>
-                        {!token ? (
-                            <div className="header__auth">
-                                <Button className="header__button-login">
-                                    <Link to="/auth/login">Đăng nhập</Link>
-                                </Button>
-                                <Button className="header__button-register">
-                                    <Link to="/auth/register">Đăng ký</Link>
-                                </Button>
-                            </div>
-                        ) : (<div className="header__auth">
-                            <BellFilled />
-                            <p>Xin chào  /</p>
-                            <Button className="header__button-register">
-                                <Link to="/auth/logout">Đăng xuất</Link>
-                            </Button>
-                        </div>)}
-                        {/* {role === "student" && (
-                            <div className="header__auth">
-                                <BellFilled />
-                                <p>Xin chào {username} /</p>
-                                <Button className="header__button-register">
-                                    <Link to="/auth/logout">Đăng xuất</Link>
-                                </Button>
-                            </div>
-                        )} */}
-                        {/* {role === "admin" && (
-                            <div className="header__auth">
-                                <Button className="header__button-admin">
-                                    <Link to="/admin/overview">Trang quản lý</Link>
-                                </Button>
-                            </div>
-                        )} */}
 
+                        <div className="header__auth">
+                            {token ? (
+                                <>
+                                    {token === "123456" ? (
+                                        <Button className="header__button-admin">
+                                            <Link to="/admin/overview">Trang quản lý</Link>
+                                        </Button>
+                                    ) : (
+                                        <>
+                                            <BellFilled style={{ marginRight: 8 }} />
+                                            <p>Xin chào /</p>
+                                            <Button className="header__button-register">
+                                                <Link to="/auth/logout">Đăng xuất</Link>
+                                            </Button>
+                                        </>
+                                    )}
+
+                                </>
+                            ) : (
+                                <>
+                                    <Button className="header__button-login">
+                                        <Link to="/auth/login">Đăng nhập</Link>
+                                    </Button>
+                                    <Button className="header__button-register">
+                                        <Link to="/auth/register">Đăng ký</Link>
+                                    </Button>
+                                </>
+                            )}
+                        </div>
                     </div>
                 </Container2>
             </div>
+
             <Outlet />
 
         </>
